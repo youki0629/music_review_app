da58e7800058ec7315aa56f7294e71472de4086f
youki0629 <youki.0629@gmail.com>
2025-09-29 06:29:43 +0900

    バリデーションエラーメッセージを表示

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,14 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!  # 未ログインなら投稿不可
+  before_action :authenticate_user!
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @reviews = current_user.reviews.order(created_at: :desc)
   end
 
   def show
-    @review = current_user.reviews.find(params[:id])
+  # @reviewはbefore_actionで設定済み
   end
 
   def new
@@ -23,7 +25,30 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+  # @reviewはbefore_actionで設定済み
+  end
+  
+  def update
+    # @reviewはbefore_actionで設定済み
+    if @review.update(review_params)
+      redirect_to @review, notice: 'レビューを更新しました'
+    else
+      render :edit  # ← editテンプレートを再表示（エラー情報付き）
+    end
+  end
+
+  def destroy
+    # @reviewはbefore_actionで設定済み
+    @review.destroy!
+    redirect_to reviews_path, notice: 'レビューを削除しました'
+  end
+
   private
+
+  def set_review
+    @review = current_user.reviews.find(params[:id])
+  end
 
   def review_params
     params.require(:review).permit(:title, :artist, :genre, :body)
